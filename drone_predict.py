9d51581cfa18e83dbd35ecfe2c9afb67f0cabb38
Ettore <ettoreitaut4.1@gmail.com>
2022-01-12 17:58:04 +0100

    Updating argparse

--- drone_predict.py
+++ drone_predict.py
@@ -1,21 +1,50 @@
 import pandas as pd
-import sys, os
-import joblib # used to save / import models
+import sys
+import os
+import joblib  # used to save / import models
 import argparse
+
+
+class bcolors:
+    HEADER = '\033[95m'
+    OKBLUE = '\033[94m'
+    OKCYAN = '\033[96m'
+    OKGREEN = '\033[92m'
+    WARNING = '\033[93m'
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
 
 clasfs = ['G1', 'G2', 'G3', 'D1', 'D2', 'D3']
 align = ['HH', 'VV']
-drones = ['F450','HELICOPTER','MAVIC','PARROT','P4P','HEXA','M100','WALKERA','Y600']
-freq = [26,28,30,32,34,36,38,40]
+drones = ['F450', 'HELICOPTER', 'MAVIC', 'PARROT',
+          'P4P', 'HEXA', 'M100', 'WALKERA', 'Y600']
+freq = [26, 28, 30, 32, 34, 36, 38, 40]
 
-parser = argparse.ArgumentParser(description='Drone Recognition from RCS signatures     -   suggested classifier per drones: KNN, 32GHz ')
-parser.add_argument("-c", "--classifiers", choices = clasfs, help = "choose classifier: <G1> Decision Tree, <G2> KNN, <G3> SGDC,  <D1> Decision Tree, <D2> KNN, <D3> SGDC",metavar='')
-parser.add_argument("-a", "--alignment", choices = align, default = align[1], help = "choose alignment: HH or VV",metavar = '')
-parser.add_argument("-m", "--model", choices = drones, default = drones[0], help = "choose drone - F450, HELICOPTER, MAVIC, PARROT, P4P, HEXA, M100, WALKERA, Y600", metavar='')
-parser.add_argument("-f", "--frequency", choices = freq, type=int, default = freq[3], help = "choose frequency: 26, 28, 30, 32, 34, 36, 38, 40 [GHz]",metavar =' ')
-parser.add_argument("-od", "--override", nargs='+', type=float, help = "manual override. Write frequency, mean, max - it works without any check")
+parser = argparse.ArgumentParser(
+    description='Drone Recognition from RCS signatures     -   suggested classifier per drones: KNN, 32GHz ')
+
+group1 = parser.add_argument_group(
+    'Data from dataset', 'Given freq, name and alignment, takes data from the dataset automatically')
+group2 = parser.add_argument_group(
+    'Manual Data', 'Manually give freq, mean, max')
+
+
+parser.add_argument("-c", "--classifiers", choices=clasfs,
+                    help="choose classifier: <G1> Decision Tree, <G2> KNN, <G3> SGDC,  <D1> Decision Tree, <D2> KNN, <D3> SGDC", metavar='', required=True)
+group1.add_argument("-a", "--alignment", choices=align,
+                    help="choose alignment: HH or VV", metavar='')
+group1.add_argument("-m", "--model", choices=drones,
+                    help="choose drone - F450, HELICOPTER, MAVIC, PARROT, P4P, HEXA, M100, WALKERA, Y600", metavar='')
+group1.add_argument("-f", "--frequency", choices=freq, type=int,
+                    help="choose frequency: 26, 28, 30, 32, 34, 36, 38, 40 [GHz]", metavar=' ')
+group2.add_argument("-od", "--override", nargs='+', type=float,
+                    help="manual override. Write frequency, mean, max - it works without any check")
 
 args = parser.parse_args()
+
 
 # ARGPARSE classifiers
 if (args.classifiers != None):
@@ -43,26 +72,33 @@ base_dir = os.path.dirname(os.path.realpath(__file__))
 filepath = base_dir + file_path
 
 # Import dataframe
-df = pd.read_excel('table2.ods') 
+df = pd.read_excel('table2.ods')
 
 # TODO TEST ARGS NOT NULL != override.
 loaded_model = joblib.load(str(filepath + model + ".sav"))
 
-if args.override == None:
-    test = df.loc[(df['Pos'] == args.alignment) & (df['Model'] == args.model) & (df['f[GHz]'] == args.frequency)]
-    X_test = test.drop(columns=['Model', 'Pos','std', 'Group'])
+if(args.override is not None) and ((args.alignment is not None) or (args.model is not None) or (args.frequency is not None)):
+    print(bcolors.FAIL +
+          "ERROR: use -a, -m, -f together, -od alone. -c is required." + bcolors.ENDC)
+    sys.exit(1)
+elif (args.override != None) and (len(args.override) != 3):
+    print(bcolors.FAIL + "3 values are needed. Exiting" + bcolors.ENDC)
+    sys.exit(1)
+else:
+    print(bcolors.OKCYAN + "OK" + bcolors.ENDC)
+
+# args -a, -m, -f for not none
+if (args.override == None):
+    test = df.loc[(df['Pos'] == args.alignment) & (
+        df['Model'] == args.model) & (df['f[GHz]'] == args.frequency)]
+    X_test = test.drop(columns=['Model', 'Pos', 'std', 'Group'])
 
     # TEST HARDCODED
     # ['P4P']
     # X_test = [[26,-15.7,0.1]]
-    print(loaded_model.predict(X_test))
-    
+    print("predicted:  " + bcolors.BOLD + bcolors.OKGREEN +
+          str(loaded_model.predict(X_test)[0]) + bcolors.ENDC)
 else:
-    if len(args.override) != 3:
-        print("3 values are needed. Exiting")
-        sys.exit(1)
-    else:
-        X_test = [[args.override[0], args.override[1], args.override[2]]]
-        print(loaded_model.predict(X_test))
-
-    
+    X_test = [[args.override[0], args.override[1], args.override[2]]]
+    print("predicted:  " + bcolors.BOLD + bcolors.OKGREEN +
+          str(loaded_model.predict(X_test)[0]) + bcolors.ENDC)
